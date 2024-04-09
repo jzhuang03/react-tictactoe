c8f7d3f7841661f333c0ebb6c3791e9b19c9b481
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-04-08 20:46:33 -0400

    corrected improved efficiency with added parameter to Square function

--- codesandbox/src/App.js
+++ codesandbox/src/App.js
@@ -1,24 +1,28 @@
-function Square(){
-  return <button className = "square"> 1</button>;
+
+/* In improving efficiency, we must add a parameter that takes in a value within each square of the grid board*/
+function Square({ value }) {
+  return <button className="square"> {value} </button>;
 }
 
+/* This function makes the board accessible to other functions (export), and will be marked as the main function to be referred (default) */
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+
       </div>
     </>
   );
