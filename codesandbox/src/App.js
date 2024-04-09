2453e347cd2cc258c91f704a698ae3f194293fa4
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-04-08 21:33:47 -0400

    each Square will now receive a value prop that will either be 'X', 'O', or null for empty squares.

--- codesandbox/src/App.js
+++ codesandbox/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+
+/* Child component */
 function Square() {
   const [value, setValue] = useState(null);
   function handleClick() {
@@ -10,24 +12,26 @@ function Square() {
 
 }
 
-/* This function makes the board accessible to other functions (export), and will be marked as the main function to be referred (default) */
+/* Parent component: This function makes the board accessible to other functions (export), and will be marked as the main function to be referred (default) */
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
 
       </div>
     </>
